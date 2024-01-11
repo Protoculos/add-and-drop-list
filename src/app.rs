9cb555dfc7f11bada47d1b0c590c3668980c7a55
Protoculos <mazdak2012@gmail.com>
2024-01-11 18:52:35 +0300

    my comp second dead

--- src/app.rs
+++ src/app.rs
@@ -1,40 +1,23 @@
 #![allow(non_snake_case)]
 
+use crate::models::item::ItemCard;
 use crate::route::Route;
 use dioxus::prelude::*;
 use dioxus_router::prelude::*;
+use dioxus_signals::*;
 
-pub fn App(cx: Scope) -> Element {
-    // let selected = use_ref(cx, Vec::new);
-    render! { Router::<Route> {} }
+#[derive(Clone, Copy, Default)]
+pub struct ApplicationData {
+    pub left_vec: Signal<Vec<ItemCard>>,
+    pub right_vec: Signal<Vec<ItemCard>>,
 }
 
-// js solution
-// https://www.youtube.com/watch?v=4AHot187Lj0
-// <script>
-// let lists = document.getElementsByClassName("list");
-// let fightBox = document.getElementById("right");
-// let leftBox = document.getElementById("left");
+pub fn use_app_data(cx: Scope) -> ApplicationData {
+    *use_context(cx).unwrap()
+}
 
-// for (list of lists){
-//     list.addEventListener("dragstart", function(e)){
-//         let selected = e.target;
+pub fn App(cx: Scope) -> Element {
+    use_context_provider(cx, ApplicationData::default);
 
-//         rightBox.addEventListener("dragover", function(e){
-//             e.preventDefault();
-//         });
-//         rightBox.addEventListener("drop", function(e){
-//             rightBox.appendChild(selected);
-//             selected = nill;
-//         });
-//         leftBox.addEventListener("dragover", function(e){
-//             e.preventDefault();
-//         });
-//         leftBox.addEventListener("drop", function(e){
-//             rightBox.appendChild(selected);
-//             selected = nill;
-//         })
-//     }
-// }
-
-// </script>
+    render! { Router::<Route> {} }
+}
--- src/ui/components/nav_bar.rs
+++ src/ui/components/nav_bar.rs
@@ -8,11 +8,8 @@ pub fn NavBar(cx: Scope) -> Element {
         div { class: "w-64 h-screen bg-slate-400",
             Link { class: "w-64 h-screen bg-slate-400", to: Route::Home {}, "Home" }
             ul {
-                "Simple lists"
                 li { class: "flex flex-col",
-                    Link { class: "", to: Route::Simple {}, "Simple List" }
-                    Link { class: "", to: Route::SimpleVec {}, "Simple Vector" }
-                    Link { class: "", to: Route::AddToVec {}, "Add to Vector" }
+                    Link { class: "", to: Route::DelFromVec {}, "Simple Vector" }
                 }
             }
         }
